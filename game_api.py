86bb42591f02d56b269d145876b6a9b6f901cbc6
aminebennaji19 <aminebenneji@gmail.com>
2022-05-01 16:44:47 +0100

    refactor(game rest api): add the game rest api

--- game_api.py
+++ game_api.py
@@ -1,10 +1,11 @@
+import logging
 import sys
 
 import flask
 import pygame
 from absl import app
 
-from utils.game import Game
+from core.utils.game import Game
 
 
 def dino_vs_robots():
@@ -13,19 +14,35 @@ def dino_vs_robots():
     screen_width = 600
     screen_height = 600
     dinosaur_shooting_timer = 800
+
+    number_of_dinosaur_rows = 3
+    number_of_dinosaur_cols = 4
+    obstacle_amount = 4
     moving_dinosaurs = False
     pygame.init()
 
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
-    game = Game(screen, screen_width, screen_height, moving_dinosaurs)
+
+    game = Game(
+        screen=screen,
+        screen_width=screen_width,
+        screen_height=screen_height,
+        number_of_dinosaur_rows=number_of_dinosaur_rows,
+        number_of_dinosaur_cols=number_of_dinosaur_cols,
+        obstacle_amount=obstacle_amount,
+        moving_dinosaurs=moving_dinosaurs,
+    )
     DINOSAURLASER = pygame.USEREVENT + 1
     pygame.time.set_timer(DINOSAURLASER, dinosaur_shooting_timer)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print(f"Final Score:{game.score}")
+
                 pygame.quit()
                 sys.exit()
+                break
             if event.type == DINOSAURLASER:
                 game.dinosaur_shoot()
 
@@ -49,4 +66,4 @@ def api_start():
 
 if __name__ == "__main__":
 
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True, use_reloader=False)
